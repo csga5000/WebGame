ac42d80004e83e73a88ba134ff2ff10d7fb91d15
Chase Anderson <chase@gurutechnologies.net>
2015-02-27 12:20:54 -0700

    Styling and grid generation

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
+//Variable declarations
 var add;
 var display;
-$(function(){
+
+//The function I created gets called when the page is loaded
+$(document).ready(function(){
 	display = $('#display').remove();
 });
 
+/**
+ * Called when submit button is pressed
+ * Loads characters as div's
+ **/
 function submitLevel(){
-	$('#getContent').after(display);
+	var content = $('#map').val();
+	var output = '';
+
+	//X and Y aren't used YET but may be usefull
+	var x = 0;
+	var y = 0;
+	for(var i = 0; i < content.length; i++) {
+		var c = content.charAt(i);
+
+		//If it's a newline
+		if (c === '\n') {
+			output += '<br>';
+			y++;
+		}
+		//If it's not an 'empty' character
+		else if(c != '.') {
+			output += '<div class="tile">' + c + '</div>';
+		}
+		//Empty Char
+		else {
+			output += '<div class="tile"></div>';
+		}
+
+		x++;
+	}
+	$('#beforeDisplay').after(display);
+	$('#display').html(output);
 }
